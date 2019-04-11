73de4f5375616d50b6c0b176a19a43543e0db874
Валентин Мазурок <valentin.mazurok@gmail.com>
2019-04-11 20:31:09 +0300

    start

---
+++ Assets/MoveScript.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class MoveScript : MonoBehaviour
+{
+    private NavMeshAgent _agent;
+    private Animator _animator;
+
+    private void Start()
+    {
+        _agent = GetComponent<NavMeshAgent>();
+        _animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit))
+            {
+                _agent.SetDestination(hit.point);
+            }
+            
+//            if (_agent.isStopped)
+//            {
+//                _animator.SetBool("Running", false);
+//            }
+//            else
+//            {
+//                _animator.SetBool("Running", true);
+//            } 
+        }
+    }
+}
